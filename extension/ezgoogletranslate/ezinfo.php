1fd858492e411588b50852cd9e83b0941c54b779
gggeek <gg@ez.no>
2009-03-26 23:21:08 +0000

    - first bits of code imported
    git-svn-id: http://svn.projects.ez.no/ezgoogletranslate/trunk@1 c71b1edb-0483-4514-8490-aa5ef0ed83ac

---
+++ extension/ezgoogletranslate/ezinfo.php
@@ -0,0 +1,19 @@
+<?php
+
+class ezgoogletranslateInfo
+{
+    static function info()
+    {
+        return array(
+            'Name' => "eZ Google Translate",
+            'Version' => "0.1",
+            'Copyright' => "Copyright (C) Gaetano Giunta 2009 ",
+            'License' => "GNU General Public License v2.0",
+            'Includes the following third-party services' => array (
+                'Google AJAX Language API' => 'Terms of use: http://code.google.com/apis/ajaxlanguage/terms.html'
+            )
+        );
+    }
+}
+
+?>
