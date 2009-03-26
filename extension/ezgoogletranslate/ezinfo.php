27fa4d9b8903f507d15f71c2dc28e1b7f1ae830c
gggeek <gg@ez.no>
2009-03-26 23:32:05 +0000

    - wording in info page
    git-svn-id: http://svn.projects.ez.no/ezgoogletranslate/trunk@4 c71b1edb-0483-4514-8490-aa5ef0ed83ac

--- extension/ezgoogletranslate/ezinfo.php
+++ extension/ezgoogletranslate/ezinfo.php
@@ -7,9 +7,9 @@ class ezgoogletranslateInfo
         return array(
             'Name' => "eZ Google Translate",
             'Version' => "0.1",
-            'Copyright' => "Copyright (C) Gaetano Giunta 2009 ",
+            'Copyright' => "Copyright (C) Gaetano Giunta 2009",
             'License' => "GNU General Public License v2.0",
-            'Includes the following third-party services' => array (
+            'Accesses the following third-party services' => array (
                 'Google AJAX Language API' => 'Terms of use: http://code.google.com/apis/ajaxlanguage/terms.html'
             )
         );
